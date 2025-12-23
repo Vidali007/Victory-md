4000c8a809342c42a5445d2efe0a6cf903345086
Vidali007 <168802688+Vidali007@users.noreply.github.com>
2025-12-23 09:31:18 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,14 +1,14 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'secured future',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "SECURED FUTURE",
+  botOwner: 'Victory vidali, // Your name
+  ownerNumber: '2348154207037', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "This Bot would make your future secured.",
   version: "3.0.5",
   updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
